93e395868ce240f0d0941e0b7e2f26b6549240eb
shubhk16 <khadeshubham74@gmail.com>
2021-04-07 12:51:29 +0530

    changes done

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,6 +1,6 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
-const color = document.querySelector(.color);
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
     // get random number between 0 - 3 
